21eccaba504e92735edb7320f5f6a9329eed6d4e
DAGMAWI-AT <vvman1717@gmail.com>
2024-03-22 23:11:50 -0700

    commit

--- Frontend/src/components/admin/dashboard/CircleBar.jsx
+++ Frontend/src/components/admin/dashboard/CircleBar.jsx
@@ -1,4 +1,4 @@
-import { CarRentalRounded, PeopleAltOutlined } from "@mui/icons-material";
+import { CarRentalRounded, EmojiPeopleOutlined, GroupWorkRounded, PeopleAltOutlined, Person3Outlined } from "@mui/icons-material";
 import React from "react";
 import { Circle } from 'rc-progress';
 import CountUp from 'react-countup'
@@ -49,12 +49,35 @@ function CircleBar({ percentage }) {
                 Total Cars
               </div>
               <div className="text-2xl text-black font-semibold  text-left">
-              <CountUp enableScrollSpy duration={2} end={100}/>
+              <CountUp enableScrollSpy duration={2} end={90}/>
               </div>
             </div>
           </div>
         </div>
         
+        <div className="lg:col-span-1 md:col-span-2 sm:col-span-2">
+          <div className="mx-auto bg-white rounded-xl shadow-lg hover:scale-105 transition duration-200">
+            <div className="flex justify-between">
+              <div className="ml-7 mt-5 text-black">
+                <GroupWorkRounded fontSize="large" />
+              </div>
+              <div className="mt-2 mr-2 text-black " style={{ height: "50px", width: "50px" }}>
+              <Circle percent={70} strokeWidth={10} strokeColor="blue"  trailWidth={8} trailColor="gold"/> 
+                <div>
+                <CountUp enableScrollSpy duration={2} end={percentage}/>%
+                </div>
+              </div>
+            </div>
+            <div className="pl-7 py-5">
+              <div className="text-blue-600 font-semibold  text-left">
+                Employers
+              </div>
+              <div className="text-2xl text-black font-semibold  text-left">
+              <CountUp enableScrollSpy duration={2} end={70}/>
+              </div>
+            </div>
+          </div>
+        </div>
         {/* Repeat the above code for other grid items as needed */}
       </div>
     </section>
