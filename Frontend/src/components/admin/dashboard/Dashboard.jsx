b9b05a5ad826cfda6177e3539a9df3e6aaef5bab
DAGMAWI-AT <vvman1717@gmail.com>
2024-03-22 22:46:50 -0700

    cimmit

--- Frontend/src/components/admin/dashboard/Dashboard.jsx
+++ Frontend/src/components/admin/dashboard/Dashboard.jsx
@@ -12,43 +12,43 @@ import {
 import CircleBar from "./CircleBar";
 const data = [
   {
-    name: "Page A",
+    name: "Month",
     uv: 4000,
     pv: 2400,
     amt: 2400
   },
   {
-    name: "Page B",
+    name: "Month",
     uv: 3000,
     pv: 1398,
     amt: 2210
   },
   {
-    name: "Page C",
+    name: "Month",
     uv: 2000,
     pv: 9800,
     amt: 2290
   },
   {
-    name: "Page D",
+    name: "Month",
     uv: 2780,
     pv: 3908,
     amt: 2000
   },
   {
-    name: "Page E",
+    name: "Month",
     uv: 1890,
     pv: 4800,
     amt: 2181
   },
   {
-    name: "Page F",
+    name: "Month",
     uv: 2390,
     pv: 3800,
     amt: 2500
   },
   {
-    name: "Page G",
+    name: "Month",
     uv: 3490,
     pv: 4300,
     amt: 2100
