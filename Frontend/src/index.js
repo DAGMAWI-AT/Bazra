fe809526a6ec76bf085c4e52af034a451753b55a
DAGMAWI-AT <vvman1717@gmail.com>
2024-04-08 03:30:12 -0700

    commit back end and front end

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {RouterProvider} from "react-router-dom";
-import router from './components/routers/router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/routers/router";
+import AuthProvider from "./Auths/AuthProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<RouterProvider router= {router} />  
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-
